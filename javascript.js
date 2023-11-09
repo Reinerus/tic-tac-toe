6b111e294accb7544a1e2fb657bd1b20b6011f7b
EthanFriedrichs <ethanpfriedrichs3@gmail.com>
2023-11-09 10:29:50 -0500

    Fixed JS

--- javascript.js
+++ javascript.js
@@ -2,80 +2,80 @@ var playerTurn = 1;
 var playerOneScore = 0;
 var playerTwoScore = 0;
 var buttons = [
-    ["-", "-", "-"]
-    ["-", "-", "-"]
+    ["-", "-", "-"], 
+    ["-", "-", "-"],
     ["-", "-", "-"]
 ]
 
 
 function setShape(x, idName) // Takes in the button and it's idName and changes it to the corresponding player's shape
 {
-    console.log(x.innerHTML, idName)
+    console.log("SETSHAPE:", x.innerHTML, idName)
 
-    if (playerTurn == 1)
+    if (playerTurn == 1 && (x.innerHTML != "X" && x.innerHTML != "O"))
     {
         x.innerHTML = "X"
 
         if (idName == "1")
-        {buttons[0][0] == "X"}
+        {buttons[0][0] = "X"}
 
         else if (idName == "2")
-        {buttons[0][1] == "X"}
+        {buttons[0][1] = "X"}
         
         else if (idName == "3")
-        {buttons[0][2] == "X"}
+        {buttons[0][2] ="X"}
 
         else if (idName == "4")
-        {buttons[1][0] == "X"}
+        {buttons[1][0] = "X"}
 
         else if (idName == "5")
-        {buttons[1][1] == "X"}
+        {buttons[1][1] = "X"}
 
         else if (idName == "6")
-        {buttons[1][2] == "X"}
+        {buttons[1][2] = "X"}
 
         else if (idName == "7")
-        {buttons[2][0] == "X"}
+        {buttons[2][0] = "X"}
 
         else if (idName == "8")
-        {buttons[2][1] == "X"}
+        {buttons[2][1] = "X"}
 
         else if (idName == "9")
-        {buttons[2][2] == "X"}
+        {buttons[2][2] = "X"}
 
         playerTurn = 2;
     }
 
-    else if (playerTurn == 2)
+    else if (playerTurn == 2 && (x.innerHTML != "X" && x.innerHTML != "O"))
     {
         x.innerHTML = "O"
 
         if (idName == "1")
-        {buttons[0][0] == "O"}
+        {buttons[0][0] = "O"}
 
         else if (idName == "2")
-        {buttons[0][1] == "O"}
+        {buttons[0][1] = "O"}
         
         else if (idName == "3")
-        {buttons[0][2] == "O"}
+        {buttons[0][2] = "O"}
 
         else if (idName == "4")
-        {buttons[1][0] == "O"}
+        {buttons[1][0] = "O"}
 
         else if (idName == "5")
-        {buttons[1][1] == "O"}
+        {buttons[1][1] = "O"}
 
         else if (idName == "6")
-        {buttons[1][2] == "O"}
+        {buttons[1][2] = "O"}
 
         else if (idName == "7")
-        {buttons[2][0] == "O"}
+        {buttons[2][0] = "O"}
 
         else if (idName == "8")
-        {buttons[2][1] == "O"}
+        {buttons[2][1] = "O"}
 
         else if (idName == "9")
-        {buttons[2][2] == "O"}
+        {buttons[2][2] = "O"}
 
         playerTurn = 1
     }
@@ -85,12 +85,13 @@ function setShape(x, idName) // Takes in the button and it's idName and changes 
 
 function check(idName) // Checks if there are any tic tac toes
 {
-    console.log(buttons)
+    console.log("CHECK:", buttons)
+    console.log("---------")
     
     // Checks row 1 horizontally
-    if ((buttons[0][0] == buttons[0][1]) && (buttons[0][1] == buttons[0][2]))
+    if ((buttons[0][0] == buttons[0][1]) && (buttons[0][1] == buttons[0][2]) && (buttons[0][0] == "X" || buttons[0][0] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "ROW 1")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -105,9 +106,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks row 2 horizontally
-    else if ((buttons[1][0] == buttons[1][1]) && (buttons[1][1] == buttons[1][2]))
+    else if ((buttons[1][0] == buttons[1][1]) && (buttons[1][1] == buttons[1][2]) && (buttons[1][0] == "X" || buttons[1][0] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "ROW 2")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -122,9 +123,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks row 3 horizontally
-    else if ((buttons[2][0] == buttons[2][1]) && (buttons[2][1] == buttons[2][2]))
+    else if ((buttons[2][0] == buttons[2][1]) && (buttons[2][1] == buttons[2][2]) && (buttons[2][0] == "X" || buttons[2][0] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "ROW 3")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -139,9 +140,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks column 1 vertically
-    else if ((buttons[0][0] == buttons[1][0]) && (buttons[1][0] == buttons[2][0]))
+    else if ((buttons[0][0] == buttons[1][0]) && (buttons[1][0] == buttons[2][0]) && (buttons[0][0] == "X" || buttons[0][0] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "VERTICAL 1")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -156,9 +157,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks column 2 vertically
-    else if ((buttons[0][1] == buttons[1][1]) && (buttons[1][1] == buttons[2][1]))
+    else if ((buttons[0][1] == buttons[1][1]) && (buttons[1][1] == buttons[2][1]) && (buttons[0][1] == "X" || buttons[0][1] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "VERTICAL 2")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -173,9 +174,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks column 3 vertically
-    else if ((buttons[0][2] == buttons[1][2]) && (buttons[1][2] == buttons[2][2]))
+    else if ((buttons[0][2] == buttons[1][2]) && (buttons[1][2] == buttons[2][2]) && (buttons[0][2] == "X" || buttons[0][2] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTM, "VERTICAL 3")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -190,9 +191,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks top left diagonally
-    else if ((buttons[0][0] == buttons[1][1]) && (buttons[1][1] == buttons[2][2]))
+    else if ((buttons[0][0] == buttons[1][1]) && (buttons[1][1] == buttons[2][2]) && (buttons[0][0] == "X" || buttons[0][0] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "LEFT DIAGONAL")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -207,9 +208,9 @@ function check(idName) // Checks if there are any tic tac toes
     }
 
     // Checks top right diagonally
-    else if ((buttons[0][2] == buttons[1][1]) && (buttons[1][1] == buttons[2][0]))
+    else if ((buttons[0][2] == buttons[1][1]) && (buttons[1][1] == buttons[2][0]) && (buttons[0][2] == "X" || buttons[0][2] == "O"))
     {
-        console.log(document.getElementById(idName).innerHTML)
+        console.log(document.getElementById(idName).innerHTML, "RIGHT DIAGONAL")
         if (document.getElementById(idName).innerHTML == "X")
         {
             playerOneScore++;
@@ -227,13 +228,19 @@ function check(idName) // Checks if there are any tic tac toes
 function resetButtons() // Sets all buttons back to "-" to signify emptyness
 {
     console.log("RESETTING BUTTONS")
-    buttons[0][0] = "-"
-    buttons[0][1] = "-"
-    buttons[0][2] = "-"
-    buttons[1][0] == "-"
-    buttons[1][1] == "-"
-    buttons[1][2] == "-"
-    buttons[2][0] == "-"
-    buttons[2][1] == "-"
-    buttons[2][2] == "-"
+    for (let i = 0; i < document.body.getElementsByTagName("button").length; i++)
+    {
+        console.log("Setting", document.body.getElementsByTagName("button")[i].innerHTML, "to ''" )
+        document.body.getElementsByTagName("button")[i].innerHTML = ""
+    }
+    buttons[0][0] = ""
+    buttons[0][1] = ""
+    buttons[0][2] = ""
+    buttons[1][0] = ""
+    buttons[1][1] = ""
+    buttons[1][2] = ""
+    buttons[2][0] = ""
+    buttons[2][1] = ""
+    buttons[2][2] = ""
+    console.log("FINAL BUTTONS:", buttons)
 }
